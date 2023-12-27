dae77f12a648907ac8960e4a3f2b8c7ca79e0dc9
OppedRawen <yud2372@gmail.com>
2023-12-26 22:14:17 -0800

    loginPage setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,11 @@
 import Image from 'next/image'
 import { db } from './lib/db';
+import Button from './components/ui/Button';
 
 export default async function Home() {
 
   await db.set('hello','hello');
   return (
-    <div>
-      yes world
-    </div>
+    <Button variant='default'>Hello</Button>
   )
 }

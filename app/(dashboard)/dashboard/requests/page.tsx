abdc6c34a9a39971df239b54ed1e3f766a850116
OppedRawen <yud2372@gmail.com>
2024-01-13 02:03:55 -0800

    parsed sender as instead of just getting it

--- app/(dashboard)/dashboard/requests/page.tsx
+++ app/(dashboard)/dashboard/requests/page.tsx
@@ -11,15 +11,20 @@ const page: FC = async ({}) => {
     if(!session) notFound();
 
     // ids of people who sent current logged in user a freiend request
-    const incomingSenderIds = (await fetchRedis('smembers',`user:${session.user.id}:incoming_friend_reqests`) )as string[];
+    const incomingSenderIds = (await fetchRedis('smembers',`user:${session.user.id}:incoming_friend_requests`) )as string[];
 
     const incomingFriendRequests = await Promise.all(incomingSenderIds.map(async(senderId)=>{
-        const sender = await fetchRedis('get',`user:${senderId}`) as User;
+    //   because this is a json string, we need to parse it instead of just getting it
+        const sender = await fetchRedis('get',`user:${senderId}`) as string;
+        const senderParsed = JSON.parse(sender) as User;
+        console.log('sender',sender);
         return {
             senderId,
-            senderEmail:sender.email,
+            senderEmail:senderParsed.email,
         }
     }))
+    // console log incomingFriendRequests email
+    console.log(incomingFriendRequests.map((request)=>request.senderEmail),incomingFriendRequests)
   return <main className='pt-8'>
   <h1 className='font-bold text-5xl mb-8'>Add a friend</h1>
     <div className='flex flex-col gap-4'>
--- app/components/ui/Friendrequests.tsx
+++ app/components/ui/Friendrequests.tsx
@@ -1,5 +1,5 @@
 'use client'
-import { Check, UserPlus } from 'lucide-react'
+import { Check, UserPlus, X } from 'lucide-react'
 import { FC, useState } from 'react'
 
 interface FriendrequestsProps {
@@ -11,6 +11,8 @@ const Friendrequests: FC<FriendrequestsProps> = ({incomingFriendRequests,session
     // doing types in a d.ts file does not require import
     const [friendRequests,setFriendRequests] = useState<IncomingFriendRequests[]>(incomingFriendRequests)
   return <>
+
+
     {friendRequests.length===0?(<p className='text-sm text-zinc-500'>Nothing to show here.</p>):(friendRequests.map((request)=>(
         <div key={request.senderId} className='flex gpa-4 items-center'>
             <UserPlus className='text-black'  />
@@ -18,9 +20,10 @@ const Friendrequests: FC<FriendrequestsProps> = ({incomingFriendRequests,session
             <button aria-label="aceept friend" className='w-8 h-8 bg-indigo-600 hover:bg-indigo-700 grid place-items-center rounded-full transition hover:shadow-md'>
             <Check className='font-semibold text-white w-3/4 h-3/4'/>    
             </button>
-                <button>
 
-                </button>
+            <button aria-label="deny friend" className='w-8 h-8 bg-red-600 hover:bg-red-700 grid place-items-center rounded-full transition hover:shadow-md'>
+            <X className='font-semibold text-white w-3/4 h-3/4'/>    
+            </button>
         </div>
     )))}
   </>

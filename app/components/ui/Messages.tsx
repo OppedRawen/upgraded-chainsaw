56ceb865ad241b6a37e5623b7dc7df1cdd0ffbb4
OppedRawen <yud2372@gmail.com>
2024-01-26 02:12:14 -0800

    chat avator

--- app/components/ui/Messages.tsx
+++ app/components/ui/Messages.tsx
@@ -1,18 +1,28 @@
 "use client"
 import { cn } from '@/app/lib/utils'
 import { Message } from '@/app/lib/validations/message'
-import { FC, useRef, useState } from 'react'
 
+import { FC, useRef, useState } from 'react'
+import {format} from 'date-fns'
+import { date } from 'zod'
+import Image from 'next/image'
 interface MessagesProps {
   initialMessages: Message[]
   sessionId: String
+  sessionImg: String|undefined|null
+  chatPartner: User
 }
 
-const Messages: FC<MessagesProps> = ({initialMessages,sessionId}) => {
+const Messages: FC<MessagesProps> = ({initialMessages,sessionId, sessionImg,chatPartner}) => {
     const[messages,setMessages] = useState<Message[]>(initialMessages)
     const scrollDownRef = useRef<HTMLDivElement|null>(null)
+
+    const formatTimeStamp=(timestamp:number)=>{
+    return format(timestamp,'HH:mm')
+    }
   return <div id='messages'className='flex h-full flex-1 flex-col-reverse gap-4 overflow-y-auto scrollbar-thumb-blue scrollbar-thumb-rounded scrollbar-track-blue-lighter scrollbar-w-2 scrolling-touch'>
 
+    
     <div ref={scrollDownRef}>
     </div>
         {messages.map((message,index)=>{
@@ -30,9 +40,22 @@ const Messages: FC<MessagesProps> = ({initialMessages,sessionId}) => {
                    })}>
                     {message.text}{''}
                     <span className='ml-2 text-xs texy-gray-400'>
-                        {message.timestamp}
+                        {formatTimeStamp(message.timestamp)}
                     </span>
+
                    </span>
+                     </div>
+                     <div className={cn('relative w-6 h-6',
+                     {
+                      'order-2':isCurrentUser,
+                      'order-1':!isCurrentUser,
+                      'invisible':hasNextMessageFromSameUser
+                     })}>
+                      <Image  fill src={
+                        isCurrentUser?(sessionImg as string):chatPartner.image}
+                        alt='Profile picture'
+                        referrerPolicy='no-referrer'
+                        className='rounded-full'/>
                      </div>
                 </div>
                  </div>
